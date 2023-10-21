0b77b4dc39980af0ca5d80ff8397b02ea23ebc86
echris10sen <chr15016@byui.edu>
2023-10-21 06:02:34 -0600

    We got it boys

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
-const postsRoute = require
+const postsRoute = require("./Routes/postsRoute")
+app.get('/', (req, res) => {
+  res.send("Hello World");
+})
 
 app.use("/posts", postsRoute)
