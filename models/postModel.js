d7041f835c55a86ff09a386965c00c83f2bb7a2a
echris10sen <chr15016@byui.edu>
2023-10-21 09:53:23 -0600

    Hopefully this change will fix the error,

--- models/postModel.js
+++ models/postModel.js
@@ -1,13 +1,13 @@
 const pool = require("../database")
 
 async function getPostsData(post_id) {
-        try {
-                const data = await pool.query(
-                        `SELECT * FROM public.posts`
-                )
-                console.log(data.rows);                
+    try {
+         const data = await pool.query(
+            `SELECT * FROM public.posts`
+        )
+        console.log(data.rows);                
         } catch (error) {
-                console.error("Error in GetPostData")
+            console.error("Error in GetPostData")
         }
 
 }
